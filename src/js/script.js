9802feabc3e0a4be1c33e47f806c88fc6654d4d3
kaic-cesar <kaic_cesar@hotmail.com>
2026-02-03 23:40:22 -0300

    implemented footer and box-shadow in header

--- src/js/script.js
+++ src/js/script.js
@@ -7,3 +7,14 @@ mobileBtn.addEventListener('click', () => {
     icon.classList.toggle('fa-times');
     icon.classList.toggle('fa-bars');
 });
+
+
+window.addEventListener('scroll', () => {
+    const header = document.getElementById('header');
+    
+    if(window.scrollY > 0) {
+        header.style.boxShadow = '0 4px 10px rgba(0, 0, 0, 0.2)'
+    } else {
+        header.style.boxShadow = 'none'
+    }
+})
